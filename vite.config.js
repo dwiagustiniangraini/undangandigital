6fa2d4aac1e21a935e886e52280c45be16ccbc27
Aditiya <aditiyaramadhansaputra08@gmail.com>
2025-08-12 20:44:39 +0700

    min img size

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/undangandigital/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteImagemin({
+      webp: { quality: 75 },
+    }),
+  ],
 });
